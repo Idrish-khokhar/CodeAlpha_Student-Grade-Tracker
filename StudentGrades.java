0cbe8729119f5ccd8c84f0e2d928ceecd40794bf
Idrish-khokhar <khanidrish786000@gmail.com>
2024-06-26 22:07:24 +0530

    intial commits

---
+++ StudentGrades.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentGrades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Double> grades = new ArrayList<>();
+
+        System.out.println("Enter student grades. Type '0' to finish:");
+
+        while (true) {
+            System.out.print("Enter grade: ");
+            double grade = scanner.nextDouble();
+            if (grade == 0) {
+                break;
+            }
+            grades.add(grade);
+        }
+
+        if (grades.size() > 0) {
+            double sum = 0;
+            double highest = grades.get(0);
+            double lowest = grades.get(0);
+
+            for (double grade : grades) {
+                sum += grade;
+                if (grade > highest) {
+                    highest = grade;
+                }
+                if (grade < lowest) {
+                    lowest = grade;
+                }
+            }
+
+            double average = sum / grades.size();
+            System.out.println("Average grade: " + average);
+            System.out.println("Highest grade: " + highest);
+            System.out.println("Lowest grade: " + lowest);
+        } else {
+            System.out.println("No grades entered.");
+        }
+
+        scanner.close();
+
+
+    }
+}
